17907a86fff46b1b392f286996c9933f3424461c
shilpi <105913922+sshilpi110@users.noreply.github.com>
2023-01-30 20:07:06 +0530

    event

--- DSA Revision Template/index.js
+++ DSA Revision Template/index.js
@@ -1,8 +1,60 @@
 
-document.querySelector("#form").addEventListener("submit" ,formFun)
+document.querySelector("#form").addEventListener("submit", formFun)
 
-let dsaArr= JSON.parse(localStorage.getItem(user)) || []
-function formFun(){
-   
-    
+let dsaArr = JSON.parse(localStorage.getItem("user")) || []
+function formFun(event) {
+    event.preventDefault()
+
+    userObj = {
+        title: document.querySelector("#title").value,
+        link: document.querySelector("#link").value,
+        difficulty: document.querySelector("#difficulty").value
+    }
+    dsaArr.push(userObj)
+    localStorage.setItem("user" ,JSON.stringify(dsaArr))
+    console.log(dsaArr);
+    displayTable(dsaArr)
+}
+
+
+function displayTable(dsaArr) {
+    document.querySelector("tbody").innerText = "";
+    dsaArr.forEach(function(elem,index) {
+
+        let tr = document.createElement("tr");
+
+        let td1 = document.createElement("td");
+        td1.innerText = elem.title;
+
+        let td2 = document.createElement("td")
+        td2.innerText = elem.link;
+
+        let td3 = document.createElement("td")
+        td3.innerText = elem.difficulty;
+
+
+        let td4 = document.createElement("td")
+
+        if (elem.difficulty == "Easy") {
+            td4.innerText = "NO"
+        }
+        else {
+            td4.innerText = "yes"
+        }
+
+
+         let btn= document.createElement("button")
+         btn.innerText="DELETE"
+
+         btn.addEventListener("click" ,dltFun)
+
+         function dltFun(){
+            dsaArr.splice(i,1)
+         }
+
+        tr.append(td1, td2, td3, td4 ,btn )
+
+        document.querySelector("tbody").append(tr);
+    }) ;
+
 }
---
+++ E-commerce-1.html/admin.js
@@ -0,0 +1,13 @@
+
+
+function addProduct(n,c,u,p,g,s){
+    this.name=n,
+    this.cat=c,
+    this.url=u,
+    this.price=p,
+    this.gender=g,
+    this.sold=s
+}
+
+document.querySelector("form").addEventListener("submit" ,myPage)
+
